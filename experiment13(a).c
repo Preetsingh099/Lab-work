617ccb9cfedb21364dd965cad9867abe3eb56228
Gaganpreetsingh <146857904+Preetsingh099@users.noreply.github.com>
2024-01-06 00:34:09 +0530

    Update ]experiment13(a).c

--- experiment13(a).c
+++ experiment13(a).c
@@ -2,24 +2,24 @@
 #include<stdlib.h>
 void main()
 {
-  int q1, q2, q3;
-char fname[30];
-FILE*ifp,*ofp;
-ifp=fopen("input.txt","r");
-ofp=fopen("output.txt","r");
-if(ifp==NULL||ofp==NULL)
-{
-printf("Error opening file.\n");
-exit(0);
-}
-while(!feof(ifp))
-{
-fscanf(ifp,"%s%d%d%d",fname,&q1,&q2,&q3);
-printf("%s%d%d%d \n",fname,q1,q2,q3);
-fprintf(".fp, %s%d%d%d \n",fname,q1,q2,q3);
-}
-printf("Copied the file\n");
-printf("\n");
-fclose(ofp);
-fclose(ifp);
+    FILE *fp=open("myfile.txt","r");
+    if(fp==NULL)
+    {
+        printf("Error opening file.\n");
+        exit(0);
+    }
+    char buffer[50]
+    while(fgets(buffer,sizeof(buffer),fp)!=NULL)
+    {
+        printf("%s",buffer);
+    }
+    fclose(fp);
+    fp=fopen("my.file.txt", "w");
+    if(fp==NULL)
+    {
+        printf("Error opening.");
+        exit(0);
+    }
+    fprintf(fp,"This is some text.");
+    fclose(fp);
 }
